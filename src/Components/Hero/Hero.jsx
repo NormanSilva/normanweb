2e4bec30ef46a51c2f18d92741e4a5f5d85fc09f
NormanSilva <n.silvazeta@gmail.com>
2022-11-02 18:34:02 -0500

    editando video hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Hero.css'
-import videoBG from '../../Recursos/herovideo.mp4'
+import videoHero from '../../Recursos/aherovideo.mp4'
+
 
 
 
 const Hero = () => {
-
+/* <video src={videoBG} autoplay loop className='videoBg' muted></video> */
 
   return (
     <div className='hero'>
-      <video src={videoBG} autoplay loop className='videoBg' muted></video>
+      <video src={videoHero} autoPlay loop className='videoBg' sound/>
       <div className='contenido'>
         <div className='text1'>Ayudo a crear productos digitales</div>
         <div className='text2'>que brinden experiencias significativas</div>
